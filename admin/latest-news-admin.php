010d463b858972defbc92637e3e734584794696a
Rafa Soler <rafasoler10@gmail.com>
2021-07-04 18:44:14 +0200

    Check if function exists

--- admin/latest-news-admin.php
+++ admin/latest-news-admin.php
@@ -3,13 +3,15 @@
 defined( 'ABSPATH' ) or die;
 
 add_action( 'init', 'ln_create_custom_post_type' );
-function ln_create_custom_post_type() {
-    $args = array (
-		'label'				=> __('News', 'latest-news'),
-		'public'			=> false,
-		'show_ui'			=> true,
-		'supports'			=> array('title', 'thumbnail', 'editor' ),
-		'menu_icon'			=> 'dashicons-format-aside',
-	);
-    register_post_type( 'news', $args );
+if ( !function_exists( 'ln_create_custom_post_type' ) ) {
+	function ln_create_custom_post_type() {
+		$args = array (
+			'label'				=> __('News', 'latest-news'),
+			'public'			=> false,
+			'show_ui'			=> true,
+			'supports'			=> array('title', 'thumbnail', 'editor' ),
+			'menu_icon'			=> 'dashicons-format-aside',
+		);
+		register_post_type( 'news', $args );
+	}
 }
