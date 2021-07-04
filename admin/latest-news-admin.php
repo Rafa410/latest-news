957bdef06717c5182cbddf3b84d6bb259f94c7b6
Rafa Soler <rafasoler10@gmail.com>
2021-07-04 18:20:51 +0200

    Register custom "news" post type

--- admin/latest-news-admin.php
+++ admin/latest-news-admin.php
@@ -2,3 +2,14 @@
 
 defined( 'ABSPATH' ) or die;
 
+add_action( 'init', 'ln_create_custom_post_type' );
+function ln_create_custom_post_type() {
+    $args = array (
+		'label'				=> __('News', 'latest-news'),
+		'public'			=> false,
+		'show_ui'			=> true,
+		'supports'			=> array('title', 'thumbnail', 'editor' ),
+		'menu_icon'			=> 'dashicons-format-aside',
+	);
+    register_post_type( 'news', $args );
+}
